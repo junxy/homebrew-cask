75674877cb1cc2cd193189fa709ee9d528bd47bc
Mr. Hsu <junxy@users.noreply.github.com>
2019-08-16 00:01:53 +0800

    Update oracle-jdk11.rb

--- Casks/oracle-jdk11.rb
+++ Casks/oracle-jdk11.rb
@@ -2,7 +2,7 @@ cask 'oracle-jdk11' do
   version '11.0.4,10:cf1bbcbf431a474eb9fc550051f4ee78'
   sha256 '79f3766105b5bb2aa1a200464de8ee8c98e7af0128a0585637f0b3daaf161d33'
   
-  url "https://download.oracle.com/otn-pub/java/jdk/#{version.before_comma}+#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}_osx-x64_bin.dmg",
+  url "https://download.oracle.com/otn/java/jdk/#{version.before_comma}+#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}_osx-x64_bin.dmg",
       cookies: {
                  'oraclelicense' => 'accept-securebackup-cookie',
                }
